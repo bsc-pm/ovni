86fb8b47176c293bcfbed2affa15a289cf74ff0a
Rodrigo Arias <rodrigo.arias@bsc.es>
2022-01-12 10:47:47 +0100

    Fix copyright years

--- test_speed.c
+++ test_speed.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Barcelona Supercomputing Center (BSC)
+ * Copyright (c) 2021-2022 Barcelona Supercomputing Center (BSC)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

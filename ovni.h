cf0ca35769395a34e8d497a3b402d310a45335c9
Rodrigo Arias <rodrigo.arias@bsc.es>
2022-08-29 17:09:21 +0200

    Add Nanos6 model version

--- ovni.h
+++ ovni.h
@@ -50,7 +50,7 @@ typedef struct json_value_t  JSON_Value;
 
 #define OVNI_STREAM_MAGIC "ovni"
 #define OVNI_STREAM_VERSION 1
-#define OVNI_MODEL_VERSION "O1 V1 T1 M1 D1 K1"
+#define OVNI_MODEL_VERSION "O1 V1 T1 M1 D1 K1 61"
 
 /* ----------------------- common ------------------------ */
 

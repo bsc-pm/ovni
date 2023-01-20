b4d5e33d6c66593b794c37502e925918220ec9a4
Rodrigo Arias <rodrigo.arias@bsc.es>
2023-01-20 15:36:02 +0100

    Add emu_args parser

---
+++ src/emu/emu_args.h
@@ -0,0 +1,15 @@
+/* Copyright (c) 2021-2023 Barcelona Supercomputing Center (BSC)
+ * SPDX-License-Identifier: GPL-3.0-or-later */
+
+#ifndef EMU_ARGS_H
+#define EMU_ARGS_H
+
+struct emu_args {
+	int linter_mode;
+	char *clock_offset_file;
+	char *tracedir;
+};
+
+void emu_args_init(struct emu_args *args, int argc, char *argv[]);
+
+#endif /* EMU_ARGS_H */

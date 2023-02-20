7bbd74cb1706478b276de29a48ee5fe34d6692cd
Rodrigo Arias <rodrigo.arias@bsc.es>
2023-02-20 14:40:47 +0100

    Add common unit test macros

---
+++ test/unit/unittest.h
@@ -0,0 +1,12 @@
+/* Copyright (c) 2023 Barcelona Supercomputing Center (BSC)
+ * SPDX-License-Identifier: GPL-3.0-or-later */
+
+#ifndef UNITTEST_H
+#define UNITTEST_H
+
+#include "common.h"
+
+#define OK(x) do { if ((x) != 0) { die(#x " failed"); } } while (0)
+#define ERR(x) do { if ((x) == 0) { die(#x " didn't fail"); } } while (0)
+
+#endif /* UNITTEST_H */

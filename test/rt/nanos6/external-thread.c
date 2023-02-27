4c7b44510a065d3692c9b702c3c1feab4ee17ea9
Rodrigo Arias <rodrigo.arias@bsc.es>
2023-02-27 13:35:04 +0100

    Remove empty tests

--- test/rt/nanos6/external-thread.c
+++
@@ -1,6 +0,0 @@
-/* Copyright (c) 2022 Barcelona Supercomputing Center (BSC)
- * SPDX-License-Identifier: GPL-3.0-or-later */
-
-/* Create a test which unblocks a task from an external thread. This
- * should break currently, as the instrumentation attempts to call
- * ovni_ev_emit from a thread without initialization. */

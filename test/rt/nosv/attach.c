ae7ef210147c4576b7162304f3534729fb5d1925
Rodrigo Arias <rodrigo.arias@bsc.es>
2023-12-12 18:50:57 +0100

    Fix attach nOS-V RT test
    The nosv_attach() function no longer needs a the type argument.

--- test/rt/nosv/attach.c
+++ test/rt/nosv/attach.c
@@ -12,23 +12,14 @@ main(void)
 {
 	nosv_init();
 
-	nosv_task_type_t type;
-	if (nosv_type_init(&type, NULL, NULL, NULL, "adopted", NULL,
-			    NULL, NOSV_TYPE_INIT_EXTERNAL)
-			!= 0)
-		die("nosv_type_init failed");
-
 	nosv_task_t task;
-	if (nosv_attach(&task, type, 0, NULL, 0) != 0)
+	if (nosv_attach(&task, NULL, NULL, 0) != 0)
 		die("nosv_attach failed");
 
 	sleep_us(100);
 
 	if (nosv_detach(0) != 0)
 		die("nosv_detach failed");
-
-	if (nosv_type_destroy(type, 0) != 0)
-		die("nosv_type_destroy failed");
 
 	if (nosv_shutdown() != 0)
 		die("nosv_shutdown failed");

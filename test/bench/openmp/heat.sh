9b373fb16c96c1ea0a254dfb461558e1223dd98a
Rodrigo Arias <rodrigo.arias@bsc.es>
2025-03-17 17:26:02 +0100

    Add bench6 test to run full mini-apps

---
+++ test/bench/openmp/heat.sh
@@ -0,0 +1,10 @@
+export NOSV_APPID=1
+export NOSV_CONFIG_OVERRIDE=instrumentation.version=ovni,ovni.level=2
+#export OMP_NUM_THREADS=1
+export OMP_OVNI=1
+
+b6_heat_ompv
+
+ovnisort ovni
+ovnitop ovni
+ovniemu -b ovni

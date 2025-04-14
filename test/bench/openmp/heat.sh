b548f2f8776ca5ec406bce53b992e5274e0a0fa9
Rodrigo Arias <rodrigo.arias@bsc.es>
2025-04-14 13:59:18 +0200

    Ensure the heat PRV traces are not too big

--- test/bench/openmp/heat.sh
+++ test/bench/openmp/heat.sh
@@ -8,3 +8,15 @@ b6_heat_ompv -b 128 -t 10
 ovnisort ovni
 ovnitop ovni
 ovniemu -b ovni
+
+# Make sure the trace is not too big (limit at 128 MiB)
+maxsize=$((128 * 1024 * 1024))
+
+for f in ovni/{cpu,thread,openmp-breakdown}.prv; do
+  size=$(stat -c %s $f)
+  if [ $size -lt $maxsize ]; then
+    echo "$f: size ok ($size < $maxsize)"
+  else
+    echo "$f: too big ($size >= $maxsize)"
+  fi
+done

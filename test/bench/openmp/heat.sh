09b84d05b16c18328fcb04b308ee1e9c919fb6a2
Rodrigo Arias <rodrigo.arias@bsc.es>
2025-04-14 13:58:42 +0200

    Run heat more times with smaller blocksize

--- test/bench/openmp/heat.sh
+++ test/bench/openmp/heat.sh
@@ -3,7 +3,7 @@ export NOSV_CONFIG_OVERRIDE=instrumentation.version=ovni,ovni.level=2
 #export OMP_NUM_THREADS=1
 export OMP_OVNI=1
 
-b6_heat_ompv
+b6_heat_ompv -b 128 -t 10
 
 ovnisort ovni
 ovnitop ovni

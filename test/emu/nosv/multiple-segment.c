ce01675e9e1478b1714b030af10c846302ba27a3
Rodrigo Arias <rodrigo.arias@bsc.es>
2025-07-23 17:22:07 +0200

    Fix nOS-V multiple-segment emu test
    The ovni_proc_set_rank() call must occur after we initialize the thread.
    It was always failing, but the multi-process driver didn't catch this
    case.

--- test/emu/nosv/multiple-segment.c
+++ test/emu/nosv/multiple-segment.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2023-2024 Barcelona Supercomputing Center (BSC)
+/* Copyright (c) 2023-2025 Barcelona Supercomputing Center (BSC)
  * SPDX-License-Identifier: GPL-3.0-or-later */
 
 #include <stdio.h>
@@ -31,8 +31,8 @@ main(void)
 		die("snprintf failed");
 
 	ovni_proc_init(1 + app, loom, getpid());
-	ovni_proc_set_rank(rank, nranks);
 	ovni_thread_init(get_tid());
+	ovni_proc_set_rank(rank, nranks);
 
 	/* Leader of the segment, must emit CPUs */
 	if (rank % N == 0) {

516b4342a22f9220f3b85efe7344bc062796f0f4
Rodrigo Arias <rodrigo.arias@bsc.es>
2025-10-21 17:06:59 +0200

    Update OmpSs-2 LLVM commit

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
       clangOmpss2Unwrapped = prev.clangOmpss2Unwrapped.override {
         useGit = true;
         gitBranch = "master";
-        gitCommit = "6ea3824988abf00ead8084994a922182bf5fd8ba";
+        gitCommit = "872ba63f86edaefc9787984ef3fae9f2f94e0124";
       };
       mpi = prev.mpich;
       bench6 = prev.bench6.overrideAttrs (old: rec {
